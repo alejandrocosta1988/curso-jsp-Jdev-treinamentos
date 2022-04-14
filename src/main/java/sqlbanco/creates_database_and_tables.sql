c6078f423145984906c86eef6f5ff4c910768c64
Alejandro Costa <alejandroesfc@gmail.com>
2022-04-14 06:46:43 -0300

    creates the database, table for login data, and inserts the admin entry to the table

---
+++ src/main/java/sqlbanco/creates_database_and_tables.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE "curso-jsp"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+ CREATE TABLE IF NOT EXISTS public.table_model_login
+(
+    login character varying(200) COLLATE pg_catalog."default",
+    senha character varying(200) COLLATE pg_catalog."default",
+    CONSTRAINT login_unique UNIQUE (login)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.table_model_login
+    OWNER to postgres;
---
+++ target/classes/sqlbanco/inserts_data_into_tables.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.table_model_login(
+	login, senha)
+	VALUES ('admin', 'admin');
